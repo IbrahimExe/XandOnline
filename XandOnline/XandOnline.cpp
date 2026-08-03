24cdcdb87cc24cf26541c638f7c12a3875d99234
Ibrahim <ibrahimmiishaq@gmail.com>
2026-08-02 18:10:58 -0700

    Update XandOnline.cpp

--- XandOnline/XandOnline.cpp
+++ XandOnline/XandOnline.cpp
@@ -3,6 +3,27 @@
 #include <WS2tcpip.h>
 #include <cstdlib>
 
+// TIC TAC TOE ONLINE
+/*
+Goal is to create a console application that can play a simple Tic Tac Toe game.
+Create a display that shows the board in the console.
+
+- Display a coordinate for the board cell display
+- Simple method is a 3x3 grid (std::cout << “[ ][ ][ ]\n”; 3 times)
+- Can make more detailed if desired
+- Use system(“cls”) to rebuild the board each step
+- Should look something like:
+ 0  1  2
+[ ][ ][ ] 0
+[ ][ ][ ] 1
+[ ][ ][ ] 2
+
+Create a server that will manage the game.
+- Listen for the client and reads responses
+- Validates the input
+- Sends updated board data to client
+
+*/
 
 int main()
 {
